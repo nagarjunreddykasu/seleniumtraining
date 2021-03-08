c0b4626aeece4c14230cbddd074b7660a126d08c
nagarjunreddykasu <nagarjun.sdet@gmail.com>
2021-03-08 21:38:43 +0530

    online

--- SeleniumTraining/src/basics/Assignment2.java
+++ SeleniumTraining/src/basics/Assignment2.java
@@ -8,8 +8,8 @@ public class Assignment2 {
 1. Write a program to find largest number among the three numbers using if else statement?
 2. Write a program to find the sum of natural numbers from 1 to 20 using while loop and for loop?
 3. Write a program to calculate based on given arithmetic operator using switch statement?
-4. Write a program to perform typecasting from int type to double type?
-5. Write a program to perform typecasting from float type to char type?
+4. Write a program to perform type casting from int type to double type?
+5. Write a program to perform type casting from float type to char type?
 6. Write a program to declare two local variables of any primitive type and print the values in console?
 7. Write a program to declare two instance variables and print the values in console?
 8. Write a program to declare two static variables and print the values in console?
