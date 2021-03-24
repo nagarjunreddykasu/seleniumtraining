b0dc72357546dcc74a332fdd10d6ece520e21edf
nagarjunreddykasu <nagarjun.sdet@gmail.com>
2021-03-24 20:56:49 +0530

    online

---
+++ SeleniumTraining/src/assignments/Assignment_6.java
@@ -0,0 +1,46 @@
+package assignments;
+
+public class Assignment_6 {
+
+}
+
+
+/*
+Polymorphism:
+-------------
+Overloading:
+1.	Write a program to define 3 instance methods with same name and different number of arguments and then call all the 3 methods in main method.
+
+2.	Write a program to define 3 static methods with same name and single argument and then call all the 3 methods in main method.
+
+3.	Write a program to define 2 instance methods and 2 static methods with same name and two arguments and then call all the 4 methods in main method.
+
+4.	Write a program to define 2 instance methods with same name and one argument of type double & short respectively and call the method with all possible types of values as parameter.
+
+Overriding:
+5.	Write programs for the following scenarios:
+a. Create a parent class (ex:Parent1) with two instance methods
+b. Create a child class (ex:Child1) by inheriting above Parent class and define the same two instance methods of above parent class and call only the child class methods using parent class reference in main method.
+
+6.	Write programs for the following scenarios:
+a. Create a parent class (ex:Parent1) with one instance method having default modifier, return type String
+b. Create a child class (ex:Child1) by inheriting above Parent class and override the same instance method of above parent class with all possible access modifiers.
+(note: comment previous method if there are more possibilities)
+
+7.	Write programs for the following scenarios:
+a. Create a parent class (ex:Parent1) with one instance method having int var-args.
+b. Create a child class (ex:Child1) by inheriting above Parent class and overload the same instance method of above parent class with single int parameter and then call the method with all possible reference types.
+c. Create a child class (ex:Child2) by inheriting above Parent class and override the same instance method of above parent class and then call the method with all possible reference types.
+
+8.	Write programs to inherit the parent class and override the instance method by using covariant return type and then call the method with all the 3 possible reference types.
+
+9.	Write programs for the following scenarios:
+a. Create a parent class (ex:Parent1) and declare two instance variables of type String with values assigned
+b. Create a child class (ex:Child1) by inheriting above Parent class and declare same two instance variables of type String with different values assigned and define one instance method with two String parameters and print the values of all variables such as local, instance & parent class variables inside this instance method, finally call the instance method in main method. (note: all the variable names should be same).
+
+10.	Write programs for the following scenarios:
+a. Create a parent class (ex:Parent1) and define one instance method
+b. Create a child class (ex:Child1) by inheriting above Parent class and override the same method and define other instance method and call the overriding & overridden methods inside this method and then invoke this new instance method in main method.
+
+
+*/
