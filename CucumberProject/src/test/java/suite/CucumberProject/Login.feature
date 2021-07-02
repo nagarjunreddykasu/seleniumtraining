05a64753ed216408c98281a89a9f0da703bac599
nagarjunreddykasu <nagarjun.sdet@gmail.com>
2021-07-02 20:43:35 +0530

    online

--- CucumberProject/src/test/java/suite/CucumberProject/Login.feature
+++ CucumberProject/src/test/java/suite/CucumberProject/Login.feature
@@ -3,18 +3,59 @@ In order to verify Login functionality
 As a user
 I want to enter username and password
 
+#Background:
+#Given user navigates to the application
+#When user validates the page title
+#
+#Scenario: In order to verify Login with valid credentials
+#And user entered "valid" username
+#And user entered "valid" password
+#Then user "shouldbe" succesfully logged in
+#
+#
+#Scenario: In order to verify Login with invalid credentials
+#And user entered "invalid" username
+#And user entered "invalid" password
+#Then user "shouldnot" succesfully logged in
+
+#To comment block of code: Ctrl+/
+
+
+#Scenario Outline: In order to verify Login with valid credentials
+#Given user navigates to the application
+#When user validates the page title
+#And user entered "<username>" username
+#And user entered "<password>" password
+#Then user "<result>" succesfully logged in
+#
+#Examples:
+#|username|password|result|
+#|nkasu@frontier.com|xeno@123|shouldbe|
+#|nagarjun@gmail.com|Inv@123	|shouldnot|
+
 Scenario: In order to verify Login with valid credentials
 Given user navigates to the application
 When user validates the page title
 And user entered valid username
 And user entered valid password
+And user selects Age category
+	|Age		 |Location|
+	|Below 18|India		|
+	|Above 18|USA			|
 Then user shouldbe succesfully logged in
 
 
-Scenario: In order to verify Login with invalid credentials
-Given user navigates to the application
-When user validates the page title
-And user entered invalid username
-And user entered invalid password
-Then user shouldnot succesfully logged in
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- CucumberProject/src/test/java/suite/CucumberProject/LoginTest.java
+++ CucumberProject/src/test/java/suite/CucumberProject/LoginTest.java
@@ -1,5 +1,9 @@
 package suite.CucumberProject;
 
+import java.util.List;
+import java.util.Map;
+
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -17,19 +21,51 @@ public class LoginTest {
 	   System.out.println("user validates the page title");
 	}
 
-	@And("user entered {word} username")
+	/*@And("user entered \"{word}\" username")
 	public void user_entered_valid_username(String username) {
 	   System.out.println("user entered "+username+" username");
 	}
 
-	@And("user entered {word} password")
+	@And("user entered \"{word}\" password")
 	public void user_entered_valid_password(String password) {
 	    System.out.println("user entered "+password+" password");
 	}
 
-	@Then("user {word} succesfully logged in")
+	@Then("user \"{word}\" succesfully logged in")
 	public void user_shouldbe_succesfully_logged_in(String validateLogin) {
 	  System.out.println("user "+validateLogin+" succesfully logged in");
+	}*/
+	
+	@And("user entered valid username")
+	public void user_entered_valid_username() {
+	   System.out.println("user entered valid username");
+	}
+
+	@And("user entered valid password")
+	public void user_entered_valid_password() {
+	    System.out.println("user entered valid password");
+	}
+	/*@And("user selects Age category")
+	public void user_selects_age_category(List<String> list) {
+	    //System.out.println("user entered "+password+" password");
+		for(int i=1;i<list.size();i++){
+			System.out.println("@And -- user selects age category: "+list.get(i));
+		}
+		
+	}*/
+	
+	@And("user selects Age category")
+	public void user_selects_age_category(DataTable table) {
+		List<Map<String,String>> list=  table.asMaps(String.class,String.class);
+		for(int i=0;i<list.size();i++){
+			System.out.println("@And -- user selects Age category: "+list.get(i).get("Age")+" Location is: "+list.get(i).get("Location"));
+		}
+		
+	}
+	//user selects Age category
+	@Then("user shouldbe succesfully logged in")
+	public void user_shouldbe_succesfully_logged_in() {
+	  System.out.println("user shouldbe succesfully logged in");
 	}
 	
 	
